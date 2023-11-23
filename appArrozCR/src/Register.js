919d8214cd5b6990a4ed4db82ca2ec68112b9938
Andres-Azofeifa <andazo2501@gmail.com>
2023-11-22 19:05:50 -0600

    Interfaz para modulo de registro

--- appArrozCR/src/Register.js
+++ appArrozCR/src/Register.js
@@ -50,7 +50,7 @@ const Register = () => {
   return (
     <View style={styles.container}>
       <Text style={{fontWeight:'bold', 'fontSize': 23}}>
-        Registro de usuario
+        Registro usuario
       </Text>
       <View style={{marginTop:40}}>
         <TextInput 
@@ -119,7 +119,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    marginTop: 100,
+    marginTop: 30,
   },
   textInput: {
     paddingTop: 20,
